113351a7ad9e2607561d51b42b8ce2c85aca8c05
guiireal <guii_real@hotmail.com>
2025-04-02 21:29:54 -0300

    new release: 3.0.0

---
+++ src/commands/member/ttp.js
@@ -0,0 +1,32 @@
+const { PREFIX } = require(`${BASE_DIR}/config`);
+const { ttp } = require(`${BASE_DIR}/services/spider-x-api`);
+const {
+  InvalidParameterError,
+} = require(`${BASE_DIR}/errors/InvalidParameterError`);
+
+module.exports = {
+  name: "ttp",
+  description: "Faz figurinhas de texto.",
+  commands: ["ttp"],
+  usage: `${PREFIX}ttp teste`,
+  handle: async ({
+    sendWaitReact,
+    args,
+    sendStickerFromURL,
+    sendSuccessReact,
+  }) => {
+    if (!args.length) {
+      throw new InvalidParameterError(
+        "Você precisa informar o texto que deseja transformar em figurinha."
+      );
+    }
+
+    await sendWaitReact();
+
+    const url = await ttp(args[0].trim());
+
+    await sendSuccessReact();
+
+    await sendStickerFromURL(url);
+  },
+};
