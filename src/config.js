d243453fb8ebf8f99bb3430f80738ac579c3aace
guiireal <guii_real@hotmail.com>
2026-02-28 11:29:51 -0300

    feat(README): update version to 7.7.0 and add Facebook download feature to the documentation feat(facebook): implement Facebook video download command and integrate it into the bot fix(config): update WhatsApp Web version to the latest fix(spider-x-api): refactor API calls to ensure consistent formatting and add Facebook download functionality chore(menu): add Facebook command to the bot's menu for user accessibility

--- src/config.js
+++ src/config.js
@@ -69,7 +69,7 @@ export const PROXY_USERNAME = "";
 export const PROXY_PASSWORD = "";
 
 // Versão do WhatsApp Web
-export const WAWEB_VERSION = [2, 3000, 1033950307];
+export const WAWEB_VERSION = [2, 3000, 1034260505];
 
 // Chave da OpenAI para o comando de suporte
 export const OPENAI_API_KEY = "";
